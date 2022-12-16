6dfb7de437dd841c2750e38f6a3289d0dde0c6e7
Ravenaw <ravenaw@gmail.com>
2022-12-16 19:31:27 +0100

    favorites go away if not logged in

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,79 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 import { Nav } from '../interfaces/navbar';
 import { Router } from '@angular/router';
+import { StorageService } from '../services/storage.service';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-
-  autohide = false
-  hide= true;
-  url ="/home";
+  isLoggedIn = false;
+  autohide = false;
+  hide = true;
+  url = '/home';
   interval: any;
   //you can access properties like this in html e.g. {{title}} or {{prop.title}}
   title = 'Flex';
   prop: Nav = {
-  title:"Test"
-  }
-  constructor(private router: Router) { }
+    title: 'Test',
+  };
+  constructor(private router: Router, private storageService: StorageService) {}
 
   ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
     this.router.events.subscribe((val) => {
       console.log(this.router.url);
       this.url = this.router.url;
-     
-    } );
+    });
   }
 
-  toggleToolbar(){
-    this.hide = ! this.hide;
-    
+  toggleToolbar() {
+    this.hide = !this.hide;
   }
 
-  mouseEnter(){
+  mouseEnter() {
     this.autohide = false;
     clearTimeout(this.interval);
   }
 
-  mouseLeave(){
+  mouseLeave() {
     this.autohide = true;
     this.setTimer();
   }
 
-  setTimer(){
+  setTimer() {
     clearTimeout(this.interval);
     this.interval = setTimeout(() => {
-      if(this.autohide){
-      this.hide = true;
+      if (this.autohide) {
+        this.hide = true;
       }
     }, 500);
   }
 
-  GoToMovies(){
-    console.log("Go to movies");
+  GoToMovies() {
+    console.log('Go to movies');
     this.router.navigate(['/', 'movies']);
   }
 
-  GoToSeries(){
-    console.log("Go to series");
+  GoToSeries() {
+    console.log('Go to series');
     this.router.navigate(['/', 'series']);
   }
 
-  GoHome(){
-    console.log("Go to Home");
+  GoHome() {
+    console.log('Go to Home');
     this.router.navigate(['/', 'home']);
   }
 
-  GoToLogin(){
-    console.log("Go to Login");
+  GoToLogin() {
+    console.log('Go to Login');
     this.router.navigate(['/', 'login']);
   }
 
-  GoToFavorites(){
-    console.log("Go to Favs");
+  GoToFavorites() {
+    console.log('Go to Favs');
     this.router.navigate(['/', 'favorites']);
   }
 }
-
