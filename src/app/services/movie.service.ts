78392fcee3686226701e8a4e4c90f55216e41720
Vlad F <vlad.faget@outlook.com>
2022-12-17 16:47:59 +0100

    nice messages

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -13,47 +13,27 @@ export class MovieService {
   api_key = 'c92599e7ae08ff763cf0d7f3d7b7590f';
   poster_path = 'https://image.tmdb.org/t/p/w500';
   Cat: Category[] = [];
-  constructor(
-    private messageService: MessageService,
-    private httpClient: HttpClient,
-    private storageService: StorageService
-  ) {
+  constructor(private messageService: MessageService, private httpClient: HttpClient, private storageService: StorageService) {
     console.log('movieservice constructor');
   }
 
   //get stuff async
   getMovies(bla?: Category): Observable<any> {
     let cat = '';
-    if (bla) {
-      cat = '&with_genres=' + bla?.id;
-    }
+    if (bla) { cat = '&with_genres=' + bla?.id; }
 
-    return this.httpClient.get(
-      'https://api.themoviedb.org/3/discover/movie?api_key=' +
-        this.api_key +
-        '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1' +
-        cat +
-        '&with_watch_monetization_types=flatrate'
-    );
+    return this.httpClient.get
+      ('https://api.themoviedb.org/3/discover/movie?api_key=' + this.api_key + '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1' + cat + '&with_watch_monetization_types=flatrate');
   }
 
   getDetails(id: number, TV: boolean): Observable<any> {
     if (TV) {
-      return this.httpClient.get(
-        'https://api.themoviedb.org/3/tv/' +
-          id +
-          '?api_key=' +
-          this.api_key +
-          '&language=en-US'
-      );
-    } else {
-      return this.httpClient.get(
-        'https://api.themoviedb.org/3/movie/' +
-          id +
-          '?api_key=' +
-          this.api_key +
-          '&language=en-US'
-      );
+      return this.httpClient.get
+        ('https://api.themoviedb.org/3/tv/' + id + '?api_key=' + this.api_key + '&language=en-US');
+    }
+    else {
+      return this.httpClient.get
+        ('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + this.api_key + '&language=en-US');
     }
   }
 
@@ -64,26 +44,21 @@ export class MovieService {
       cat = '&with_genres=' + bla?.id;
     }
 
-    return this.httpClient.get(
-      'https://api.themoviedb.org/3/discover/tv?api_key=' +
-        this.api_key +
-        '&language=en-US&sort_by=popularity.desc&page=1' +
-        cat +
-        '&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0'
-    );
+    return this.httpClient.get
+      ('https://api.themoviedb.org/3/discover/tv?api_key=' + this.api_key + '&language=en-US&sort_by=popularity.desc&page=1' + cat + '&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0');
   }
 
   getCategories(): Observable<Category[]> {
     return this.httpClient.get<Category[]>(
       'https://api.themoviedb.org/3/genre/movie/list?api_key=' +
-        this.api_key +
-        '&language=en-US',
+      this.api_key +
+      '&language=en-US',
       { responseType: 'json' }
     );
   }
 
   addFavorite(movie_id: number, is_TV: boolean): Observable<any> {
-    let user = this.storageService.getUser()[0];
+    let user = this.storageService.getUser();
     console.log(user.id);
     return this.httpClient.post(
       'https://flex-api-45ah.onrender.com/favorites',
@@ -96,7 +71,7 @@ export class MovieService {
   }
 
   removeFavorite(movie_id: number): Observable<any> {
-    let user = this.storageService.getUser()[0];
+    let user = this.storageService.getUser();
     console.log(user);
     return this.httpClient.delete(
       'https://flex-api-45ah.onrender.com/favorites/' + user.id + "/" + movie_id
@@ -104,15 +79,15 @@ export class MovieService {
   }
 
   getFavorites(): Observable<any[]> {
-    let user = this.storageService.getUser()[0];
+    let user = this.storageService.getUser();
     console.log(user);
     return this.httpClient.get<any[]>(
       'https://flex-api-45ah.onrender.com/favorites/' + user.id
     );
   }
 
-  isFavorite(movie_id:number): Observable<any>{
-    let user = this.storageService.getUser()[0];
+  isFavorite(movie_id: number): Observable<any> {
+    let user = this.storageService.getUser();
     console.log(user);
     return this.httpClient.get(
       'https://flex-api-45ah.onrender.com/favorites/' + user.id + "/" + movie_id);
